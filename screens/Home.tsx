6fc62faf08f7c8a118bf0e63877e53af49502438
Nart Aliti <alitinart@gmail.com>
2023-10-30 17:20:01 +0100

    Added Markers and a way to focus on them on press

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,62 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import { lazy, useEffect, useRef, useState } from "react";
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE, Region } from "react-native-maps";
 import * as Location from "expo-location";
+
+const busMarker = require("../assets/images/bus_stacion_marker.png");
+
+interface Station {
+  title: string;
+  location: Region;
+  description: string;
+}
 
 export default function Home() {
   const [location, setLocation] = useState<Location.LocationObject>();
+  const [region, setRegion] = useState<Region>();
+
+  const mapRef = useRef<MapView>(null);
+
+  // Would be fetched from backend...
+  const busStations: Station[] = [
+    {
+      title: "Stacioni Termokos",
+      location: {
+        latitude: 42.6528319,
+        longitude: 21.1744671,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      },
+      description: "Nenstactioni i Termokosit",
+    },
+  ];
+
+  function zoomInMarker(location: Region) {
+    if (mapRef.current == null) {
+      return;
+    }
+    mapRef.current.animateToRegion(location);
+  }
+
+  function showStactionMarkers() {
+    return busStations.map((staction, index) => {
+      return (
+        <Marker
+          key={index}
+          identifier={index.toString()}
+          coordinate={staction.location}
+          title={staction.title}
+          description={staction.description}
+          image={Platform.OS == "android" && busMarker}
+          onPress={() => zoomInMarker(staction.location)}
+        >
+          {Platform.OS == "ios" && (
+            <Image source={busMarker} style={{ width: 30, height: 30 }} />
+          )}
+        </Marker>
+      );
+    });
+  }
 
   useEffect(() => {
     (async () => {
@@ -16,20 +68,28 @@ export default function Home() {
 
       let loc = await Location.getCurrentPositionAsync();
       setLocation(loc);
+      setRegion({
+        latitude: loc.coords.latitude,
+        longitude: loc.coords.longitude,
+        longitudeDelta: 0.01,
+        latitudeDelta: 0.01,
+      });
     })();
   }, []);
 
   return location ? (
     <View style={styles.container}>
       <MapView
-        initialRegion={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          longitudeDelta: 0.01,
-          latitudeDelta: 0.01,
-        }}
+        provider={PROVIDER_GOOGLE}
+        ref={mapRef}
+        initialRegion={region}
+        region={region}
+        zoomEnabled={true}
+        showsUserLocation={true}
         style={styles.map}
-      ></MapView>
+      >
+        {showStactionMarkers()}
+      </MapView>
     </View>
   ) : (
     <Text>No Location permissions were given</Text>
